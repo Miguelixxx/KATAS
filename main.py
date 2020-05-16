ea27023822fcc6a9ca1fe63ca629ad1276087318
Miguelix <miguelixxx@gmail.com>
2020-05-16 10:20:35 +0200

    Test

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+Hola esto es un comentario
+'''
+
+# Variables (tambien es un comentario)
+
+nombre = 'Miguel F. '
+edad = 14
+esta_Trabajando = True # False
+
+dato = input ('introduce un Dato: ')
+
+if( edad >= 18 ):
+    nombre2 = input('Tu nombre: ')
+    print(nombre2 + ' ' + 'eres mayor de edad')
+else:
+        print('Eres un crio')
+
